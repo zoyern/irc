288d95b060bc5e9fba9d63ff34af33d706f0bd9a
Karimou Cisse <kcisse@f2r7s23.clusters.42paris.fr>
2025-11-07 22:33:28 +0100

    add change in code

--- includes/irc/Channel.hpp
+++ includes/irc/Channel.hpp
@@ -13,8 +13,60 @@
 #pragma once
 #include "../irc.hpp"
 
-class Lobby {
-public:
-	Lobby();
-	~Lobby();
+class Channel
+{
+	public:
+		Channel(void);
+		Channel(std::string name);
+		Channel(const Channel& other);
+		Channel& operator=(const Channel& other);
+		Channel& name(const std::string& name);
+		Channel& password(const std::string& pass);
+		Channel& op(const std::string& name);
+		Channel& size(const std::string& address);
+		Channel& invite_only(const std::string &msg);
+		Channel& restricted_topic (int max);
+		Channel& topic(int reserved);
+		~Channel(void);
+	private:
+		std::string _name;
+		std::string _password;
+		int _op;
+		int _size;
+		bool _invite_only;
+		bool _restricted_topic;
+		std::string _topic;
 };
+
+// SERVERR -> creation d'un client , creation d'un channel
+// main -> commandes globales, commandes de channel que tu veux
+
+// .hook(ON_START, &start, NULL)
+//     .hook(ON_UPDATE, &update, NULL)
+// .hook(ON_SHUTDOWN, &shutdown, NULL)
+// .hook(ON_CONNECT, &on_connect, NULL)
+// .hook(ON_DISCONNECT, &on_disconnect, NULL)
+
+// Channel &Server::channel(const std::string &name, bool is_default) {
+//     std::pair<std::map<std::string, Channel>::iterator, bool> channel =
+//     _channels.insert(std::pair<std::string, Channel>(name, Channel(name)));
+//     if (is_default) { console.log(INFO, CHANNEL_SET_DEFAULT + name); _default_channel = channel.first->second; }
+//     channel.second ? console.log(INFO, CHANNEL_CREATED + name) : console.log(INFO, CHANNEL_EXISTS + name);
+//     return (channel.first->second);
+// }
+
+// Channel(name)
+// std::string _name
+// std::string _password
+// int op; // operateur du channel a voir si on fais un truc en mode il peux y avoir des privilége different donc plusieurs operateur avec des privilége different bonnus
+// int size;
+// bool invite_only;
+// bool restricted_topic
+
+// Channel&    name(const std::string& port);
+// Channel&    password(const std::string& pass);
+// Channel&    op(const std::string& name);
+// Channel& size(const std::string& address);
+// Channel& invite_only(const std::string &msg);
+// Channel& restricted_topic (int max);
+// Channel& topic(int reserved);
--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -1,12 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Channel.cpp                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/07 20:47:38 by marvin            #+#    #+#             */
-/*   Updated: 2025/11/07 20:47:38 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <irc.hpp>
 
+Channel::Channel(void) :_name("NULL")
+{}
+Channel::Channel(std::string name): _name(name)
+{return;}
+Channel::Channel(const Channel& other);
+Channel& Channel::operator=(const Channel& other);
+Channel& Channel::name(const std::string& name)
+{
+	_name = name;
+}
+Channel& Channel::password(const std::string& pass)
+{
+	_password = pass;
+}
+// Channel& Channel::op(const std::string& name)
+// {
+// 	_op =
+// }
+// Channel& Channel::size(const std::string& address);
+// Channel& Channel::invite_only(const std::string &msg);
+// Channel& Channel::restricted_topic (int max);
+// Channel& Channel::topic(int reserved);
+Channel::~Channel(void){return;}
